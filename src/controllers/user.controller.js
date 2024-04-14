5852eaba8f9c9488b9d34bc3bcecf46e9f15a968
Saimr khan <itssameer75@gmail.com>
2024-04-14 13:40:37 +0530

    Add getUserCurrentProfile endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -345,6 +345,79 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
   .json(new ApiResponse(200, "User cover image updated successfully", user))
 })
 
+const getUserCurrentProfile = asyncHandler(async (req, res) => {
+  const {username} = req.params;
+
+  if(!username?.trim()) {
+    throw new ApiError(400, "Username is missing")
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase()?.trim()
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers"
+        },
+        subscribedToCount: {
+          $size: "$subscribedTo"
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        username: 1,
+        fullName: 1,
+        email: 1,
+        avatar: 1,
+        coverImage: 1,
+        subscribersCount: 1,
+        subscribedToCount: 1,
+        isSubscribed: 1
+
+      }
+    }
+  ])
+
+  if(!channel?.length) {
+    throw new ApiError(404, "Channel not found")
+  }
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, "Channel fetched successfully", channel))
+
+})
+
+
+
 export { 
   userSignup, 
   userLogin, 
@@ -355,4 +428,5 @@ export {
   updateUserDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserCurrentProfile,
 };
